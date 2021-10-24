3f39e74bf71e4438e1b04a48a83c8997553e2a59
ronpechuk <rpechuk@gmail.com>
2021-10-24 01:34:37 -0700

    changed bot look

--- tahelper/src/App.js
+++ tahelper/src/App.js
@@ -1,17 +1,33 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 import { DirectLine } from 'botframework-directlinejs';
 import ReactWebChat from 'botframework-webchat';
 import Navbar from './components/Navbar';
-var directLine = new DirectLine({
-   secret: '10o3qCCaNnM.wowdh1jFid6DGI0n-HIjnrW5hWfJiLivivCd6y5mOO8'
- });
 
 function App() {
+  var directLine = new DirectLine({
+     secret: '8e_eWeA1pbU.AWfQ_i8ovpyxFC88uLv6Nx4o9_biIsdz98VkwOcAMkQ'
+  });
+
   return (
     <div className="App">
       <Navbar />
-      <ReactWebChat directLine={ directLine } username='test' />
+      <ReactWebChat directLine={ directLine } styleOptions={{
+        bubbleBackground: '#ffffff',
+        backgroundColor: '#242526',
+        bubbleBorderRadius: 20,
+        bubbleBorderColor: '#e67e00',
+        bubbleBackground: '#e67e00',
+        bubbleNubSize: 10,
+        bubbleNubOffset: 'bottom',
+        bubbleTextColor: 'white',
+        sendBoxBackground: '#3a3b3c',
+        sendBoxBorderTop: 'solid 1px #3a3b3c',
+        sendBoxTextColor: 'white',
+        rootHeight: (window.innerHeight * 0.91) + 'px',
+        rootWidth: '100%'
+      }} />
     </div>
   );
 }
